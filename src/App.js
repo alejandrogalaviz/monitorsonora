f880cef3a42fb5416530c17e6038f0ed8ec9d494
Alejandro Galaviz <alejandrogalavizsalazar@gmail.com>
2020-05-18 18:17:38 -0700

    news testing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Router } from "@reach/router";
 import MapChart from "../src/components/MapChart";
 import {
@@ -8,26 +8,56 @@ import {
   DetailsChart,
 } from "../src/components/Charts";
 import { GlobalStyle } from "../src/components/GlobalStyle";
-import DataPanel from "../src/components/DataPanel";
+import DataPanel from "../src/components/DataPanel_test";
 import Noticias from "../src/components/Noticias";
+import classnames from "classnames";
 
-export default function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <main>
-        <DataPanel />
-        <section>
-          <Router>
-            <MapChart path="/" />
-            <ConfirmedChart path="/contagios" />
-            <ConfirmedByProvinceChart path="/contagios-municipio" />
-            <AccumulatedChart path="/casos-totales" />
-            <DetailsChart path="/detalles" />
-            <Noticias path="/noticias" />
-          </Router>
-        </section>
-      </main>
-    </>
-  );
+export default class App_test extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isNews: false,
+    };
+    this.openNews = this.openNews.bind(this);
+    this.closeNews = this.closeNews.bind(this);
+  }
+
+  openNews = () => {
+    this.setState({
+      isNews: true,
+    });
+  };
+
+  closeNews = () => {
+    this.setState({
+      isNews: false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <main>
+          <DataPanel isNews={this.state.isNews} />
+          <section
+            className={classnames({ newsSection: this.state.isNews === true })}
+          >
+            <Router>
+              <MapChart path="/" />
+              <ConfirmedChart path="/contagios" />
+              <ConfirmedByProvinceChart path="/contagios-municipio" />
+              <AccumulatedChart path="/casos-totales" />
+              <DetailsChart path="/detalles" />
+              <Noticias
+                openNews={this.openNews}
+                closeNews={this.closeNews}
+                path="/noticias"
+              />
+            </Router>
+          </section>
+        </main>
+      </>
+    );
+  }
 }
